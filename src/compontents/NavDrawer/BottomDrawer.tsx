3c1f0abb8cc001bdb45629d7c628cdf92997c2b8
Water.Li <lizongbin@hotmail.com>
2021-08-11 15:45:58 +0800

    Update BottomDrawer.tsx

--- src/compontents/NavDrawer/BottomDrawer.tsx
+++ src/compontents/NavDrawer/BottomDrawer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { Paper, SvgIcon, useTheme } from '@material-ui/core';
+import { Button, Paper, SvgIcon, useTheme } from '@material-ui/core';
 
 export const BottomDrawer = () => {
   const [times, setTimes] = React.useState(0);
@@ -37,14 +37,11 @@ export const BottomDrawer = () => {
         showLabels
       >
 
-        <BottomNavigationAction label={'more'}
-          icon={
-            <SvgIcon>
-              <path fill="currentColor" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
-            </SvgIcon>
-          } 
+        <button 
           onClick = {toggleOpenMore} 
-        />
+        >
+          More
+        </button>
       </BottomNavigation>
       {/*<BottomMoreNavigation open = {moreOpen} onClose = {handleCloseMore} />*/}
     </Paper>
